fdec49694fdc1d89b9d8b7f2b83c5e2801b5b0c0
unknown <tgdloch3@.corproot.net>
2012-05-07 05:55:36 +0200

    some change to be more flexible with argument

--- anydb/examples/connect.inc.php
+++ anydb/examples/connect.inc.php
@@ -4,21 +4,14 @@ require_once '../anyDB.php';
 require_once '../addon/DBHelper.php';
 require_once '../addon/QueryHelper.php';
 
-$database = 'kjoff_mitdaten';
-$host = 'kjoff_mitdaten';
+//$database = 'kjoff';
+$host = $database;
 $user = '';
 $password = '';
 $dbType = 'mysql';
+echo "-- connecting to :".$database."\n";
 
-// create a new db layer
-//$db = anyDB::getLayer('MYSQL','', $dbType);
-    //$db = anyDB::getLayer('PEAR', 'c:/php4/pear/', $dbType);
-    //$db = anyDB::getLayer('PHPLIB', '../../../inc/phplib-7.2d/', $dbType);
-    //$db = anyDB::getLayer('METABASE', '../../../inc/metabase/', $dbType);
-    //$db = anyDB::getLayer('ADODB', '../../../inc/adodb/', $dbType);
-    //$db = anyDB::getLayer('DBX','../../../inc/dbx/', $dbType);
-    $db = anyDB::getLayer('ODBC','',$dbType);
+$db = anyDB::getLayer('ODBC','',$dbType,$debug);
 
-//connect to db
 $db->connect($host, $database, $user, $password);
 ?>
--- anydb/examples/exportToSql.php
+++ anydb/examples/exportToSql.php
@@ -1,9 +1,36 @@
 <?php
+if (preg_match("/--help|-h|-help/",$argv[1])){
+  usage();
+  exit(1);
+}
+
+
+if (isset($argv[1])){
+  $database = $argv[1];
+}else{
+  $database = "kjoff_mitdaten";
+}
+
+if (isset($argv[2])){
+  $debug = $argv[2];
+}else{
+  $debug = false;
+}
+
+
 require "connect.inc.php";
 require "../addon/Exporter.php";
 
+function usage(){
+  echo "Usage: php.exe exportToSql.php [<odbcdatasource>] [<debug>]\n";
+  echo "\n";
+  echo "     Options:\n";
+  echo "         odbcdatasource : e.g. kjoff, default is kjoff_mitdaten\n";
+  echo "         debug          : true, default is false\n";
+}
+
 // export table content as sql statements
-$sqlData = Exporter::getDB($db, ANYDB_DUMP_SQL);
+$sqlData = Exporter::getDB($db, ANYDB_DUMP_SQL,true);
 
 foreach($sqlData as $key => $data) {
     //echo "$key<br>";
